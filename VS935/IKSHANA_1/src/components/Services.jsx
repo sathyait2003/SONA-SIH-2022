34099af4e8603f436828e45c956977c9986dbb0b
SaiKiran Billa <81307623+saikiranbilla@users.noreply.github.com>
2022-08-26 17:52:19 +0530

    Add files via upload

---
+++ VS935/IKSHANA_1/src/components/Services.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import serviceapi from "../API/serviceapi";
+import '../App.css'
+
+function Services() {
+    const [serviceData, setServiceData] = useState(serviceapi);
+    return (
+        <>
+            <section className="service-main-container">
+                <div className="container service-container">
+                    <h1 className="main-heading text-center fw-bold">
+                        SERVICES
+                    </h1>
+                    <div className="row">
+                        {serviceData.map((curElem) => {
+                            const { id, logo, title, info } = curElem;
+                            return (
+                                <>
+                                    <div
+                                        className="col-11 col-lg-4 col-xxl-4 work-container-subdiv"
+                                        key={id}>
+                                        <i className={`fontawesome-style ${logo}`}></i>
+                                        <h2 className="sub-heading">{title}</h2>
+                                        <p className="main-hero-para">{info}</p>
+                                    </div>
+                                </>
+                            );
+                        })}
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Services;
